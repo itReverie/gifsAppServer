b293e4379d9751708dba454a78e9d8f919c116bd
itReverie <brenda@itreverie.com>
2018-07-17 14:22:41 -0400

    Adding CORS in the app.use

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const bodyParser = require('body-parser');
 
 const messageTypes = [
   'ADD_FAVORITE_GIF',
@@ -13,34 +12,28 @@ const messageTypes = [
 }, {})
 
 
-// our localhost port
-const port = process.env.PORT ||  5000;
-
+const port=process.env.PORT ||  5000;
 const app = express()
+app.set('port', port );
+app.use((err, req, res, next) => {
+  logger.error({err})
 
-//--------------NO SERVER---------------
-app.set('port', port);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 
-//Parses the text as URL encoded smtp
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+  if (!res.headersSent) {
+    res.status(500).send()
+  }
+})
 
 // our server instance
 const server = http.createServer(app)
 
+
 // This creates our socket using the instance of the server
 const io = socketIO(server)
-
 io.origins(['https://gifsappclient.herokuapp.com/']);
-
-// This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
   console.log('New client connected')
  
@@ -63,15 +56,5 @@ io.on('connection', socket => {
 })
 
 
-app.use((err, req, res, next) => {
-  logger.error({err})
-
-
-  if (!res.headersSent) {
-    res.status(500).send()
-  }
-})
-
-//app.listen(app.get('port'));
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
