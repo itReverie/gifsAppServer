fc3bbc3ad689a88468e912c0c116eb357a8e4601
itReverie <brenda@itreverie.com>
2018-07-17 12:59:00 -0400

    Adding CORS

--- server.js
+++ server.js
@@ -19,17 +19,17 @@ const port = process.env.PORT ||  5000;
 const app = express()
 
 //--------------NO SERVER---------------
-// app.set('port', port);
+app.set('port', port);
 
-// //Parses the text as URL encoded smtp
-// app.use(bodyParser.json()); // support json encoded bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+//Parses the text as URL encoded smtp
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 // our server instance
