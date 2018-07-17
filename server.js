b61edcbfc59af01cb35884fb192a6ee12f97ad81
itReverie <brenda@itreverie.com>
2018-07-17 13:17:54 -0400

    Adding origin to socket.io

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ const server = http.createServer(app)
 // This creates our socket using the instance of the server
 const io = socketIO(server)
 
+io.origins(['https://gifsappclient.herokuapp.com/']);
+
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
   console.log('New client connected')
